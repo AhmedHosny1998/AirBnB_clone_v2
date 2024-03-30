212c51e65176089ac9ba8a219e16fca95e7280b6
Ahmed Hosny <hosny4620@gmail.com>
2024-03-31 01:59:08 +0200

    web

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Start a Flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Return hello hbnb"""
+
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """return hbhnb"""
+
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+     """Return C followed by text"""
+     
+     return 'C %s' % text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
